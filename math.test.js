2b177cce17665b2eabc9c85a0928df07b3a88b3a
BlackOnix39 <kozhushko2408@mail.ru>
2025-04-09 15:54:51 +0200

    update

--- math.test.js
+++ math.test.js
@@ -1,9 +1,27 @@
-const { returnAddComponents, returnDivideComponents } = require("./math");
+const { 
+    returnAddComponents, 
+    returnDivideComponents,
+    returnMultiplyComponents,
+    returnSubstractComponents,
+    returnFactorial 
+} = require("./math");
 
 test ( 'adds 2 + 3 to equal 5', () => {
     expect (returnAddComponents(2,3)).toBe (5);
 });
 
 test ( 'divide 10 / 5 to equal 2', () => {
-    expect (returnDivideComponents (10, 5)).toBe (2);
+    expect (returnDivideComponents (10, 0)).toBe (2);
+});
+
+test ( 'multiply 5 * 8 to equal 40', () => {
+    expect (returnMultiplyComponents (5, 8)).toBe (40);
+});
+
+test ( 'substract 83 - 26 to equal 57', () => {
+    expect (returnSubstractComponents (83, 26)).toBe (57);
+});
+
+test ( 'computes factorial correctly', () => {
+    expect ( returnFactorial (5)).toBe (120);
 });
